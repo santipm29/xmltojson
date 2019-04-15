382f21f96c76b67aa3f4a69801197ecee9f4e9ca
santipm29 <santipmartinez@outlook.com>
2019-04-15 17:22:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Transformación de XML a JSON
+
+## Requerimientos
+
+Nodejs
+
+## Dependencias
+
+```bash
+npm install 
+```
+
+## Ejecutar
+
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+
+let fs = require('fs');
+let parser = require('xml2js').parseString;
+let options = {ignoreAttrs : true, explicitArray : false};
+
+fs.readFile(__dirname + '/fact.xml','UTF-8',function(err, data) {
+    parser(data, options ,function (err, result) {
+        console.log(JSON.stringify(result));   
+    });
+});
